546f9ff605f0c3151c67391d2fd4873de9a9dca7
Søren Rune Nissen <4851097+SRNissen@users.noreply.github.com>
2024-01-24 13:10:19 +0100

    noncompiling example

---
+++ testcategory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace testcategory;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+		Console.WriteLine("Setup");
+	}
+
+	[Test]
+	public void TestWithoutCategory()
+	{
+		Console.WriteLine("TestWithoutCategory");
+		Assert.Pass();
+	}
+	
+	[Test]
+	[Category("integration")]
+	public void TestCategoryA()
+	{
+		Console.WriteLine("TestCategoryA");
+		Assert.Pass();
+	}
+	
+	[Test]
+	[TestCategory("Unit")]
+	public void TestCategoryB()
+	{
+		Console.WriteLine("TestCategoryB");
+		Assert.Pass();
+	}
+}
