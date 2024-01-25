c6a982cb8761c06d2c823df501fdefd17ef15fb2
SRNissen <4851097+SRNissen@users.noreply.github.com>
2024-01-25 07:50:10 +0100

    warninglist

---
+++ warninglist/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace warninglist;
+
+public class Tests
+{
+	public string Empty {get;set;}
+
+	[SetUp]
+	public void Setup()
+	{
+		Empty = "Hello world!";
+	}
+
+	[Test]
+	public void TestWithoutCategory()
+	{
+		Assert.Pass(Empty);
+	}
+}
